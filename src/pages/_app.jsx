022b90ee3fba412302ad4b18cf3f0d5908da71ab
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-09-28 12:11:49 +0700

    change to jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,9 @@
+/** @jsx jsx */
+import { jsx  } from 'theme-ui'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
 
 export default MyApp
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { jsx  } from 'theme-ui'
+
+const index = () => {
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+export default index
